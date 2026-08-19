73d0e30dad858533cf698ca3cb7bd6cf4f559501
Naga devi Barthala <nagadevibarthala@gmail.com>
2026-08-19 10:31:18 +0530

    Remove Google API integration and load dotenv

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-from dotenv import load_dotenv
-from google import genai
-import os
-
-load_dotenv()
-
-client = genai.Client(
-    api_key=os.getenv("GOOGLE_API_KEY")
-)
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Hi Gemini"
-)
 from voice import run_assistant
 
 
 if __name__ == "__main__":
     run_assistant()
-
